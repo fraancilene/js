87c637d5373883b2c9339da22ad2ea43a8c66b7a
Francilene Silva <francilenesilva.fps10@gmail.com>
2021-12-02 15:22:42 +0000

    js init

---
+++ Fundamentos/truthy-falsy.js
@@ -0,0 +1,7 @@
+const usuarioLogado = true
+const contaPaga = false
+
+// truthy ou falsy - não sãoboleanos mas se comportam como se fosse
+// 0 = false e 1 = true
+
+console.log(0 == false)
---
+++ Fundamentos/variaveis.js
@@ -0,0 +1,9 @@
+/** Existem 3 tipos de declaração de variáveis em JS
+ * var: declara uma variavel, opcionalmente, inicializando-a com um valor
+ * let: declara uma variável local de escopo do bloco, opcionalmente, inicializando-a com um valor
+ * const: declara uma constante de escopo de bloco, apenas de leitura.
+ */
+var altura = 5
+var comprimento = 7;
+area = altura * comprimento;
+console.log(area)
